a82197730488dbf38d9fefb9c259955dae3ba8cc
Paudric Smith <75310103+PaudricSmith@users.noreply.github.com>
2024-05-09 10:54:18 +0100

    Added an Employee Overview page that displays all employees and added more bootstrap.

---
+++ BlazorShopHRM.App/Pages/EmployeeOverview.razor.cs
@@ -0,0 +1,15 @@
+﻿using BlazorShopHRM.App.Models;
+using BlazorShopHRM.Shared.Domain;
+
+namespace BlazorShopHRM.App.Pages
+{
+    public partial class EmployeeOverview
+    {
+        public List<Employee> Employees { get; set; } = default!;
+
+        protected override void OnInitialized()
+        {
+            Employees = MockDataService.Employees;
+        }
+    }
+}
